29c5a5037df17b5d833bd12e5ea9ca78e9db6949
MohaK <mohakhalili@outlook.com>
2024-12-20 16:10:41 +0100

    finished favorite component

---
+++ src/components/Favorite.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  isFavored: boolean;
+  onFavor(): void;
+}
+
+function Favorite({ isFavored, onFavor }: Props) {
+  let classes = "clickable fa-star fa-";
+  classes += isFavored ? "solid" : "regular";
+
+  return <i className={classes} onClick={onFavor} />;
+}
+
+export default Favorite;
