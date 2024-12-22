32138a39240a0e80a51403d14f577c5ee679187a
MohaK <mohakhalili@outlook.com>
2024-12-22 17:37:29 +0100

    added bootstrap listgroup ui

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,39 @@
+interface Item {
+  _id: string;
+  name: string;
+}
+
+interface Props<T extends Item> {
+  items: T[];
+  selectedItem: T;
+  onItemSelect(category: T): void;
+}
+
+//Jämfört med andra reusable components som pagination så
+// behövde vi inte köra generics eftersom typerna är primitiva
+// Listgroup däremot kan behöva katerogirer, kunder, regioner och de typerna ser annorlunda ut.
+// Favorite kommer alltid vara en boolean och en funktion, kommer aldrig vara en number eller en kategori.
+
+function ListGroup<T extends Item>({
+  items,
+  selectedItem,
+  onItemSelect,
+}: Props<T>) {
+  return (
+    <ul className="list-group ">
+      {items.map((item) => (
+        <li
+          key={item._id}
+          onClick={() => onItemSelect(item)}
+          className={`list-group-item ${
+            item._id === selectedItem._id ? "active" : ""
+          }`}
+        >
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ListGroup;
