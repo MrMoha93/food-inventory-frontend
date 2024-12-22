2d9e643caf8fcf9b3d3f8cc86e71fd1b8bb0926c
MohaK <mohakhalili@outlook.com>
2024-12-22 17:36:18 +0100

    added range utility function

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+function range(startNumber: number, endNumber: number): number[] {
+  let array: number[] = [];
+
+  for (let count = startNumber; count <= endNumber; count++) {
+    array.push(count);
+  }
+
+  return array;
+}
+
+export default range;
