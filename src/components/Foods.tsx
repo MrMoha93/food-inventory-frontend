bdc76c5cb4c71ec6a8a34c2370f08902d10be601
MohaK <mohakhalili@outlook.com>
2024-12-22 17:38:00 +0100

    updated foods

--- src/components/Foods.tsx
+++ src/components/Foods.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 import { getFoods } from "../services/fakeFoodService";
 import Favorite from "./Favorite";
+import Pagination from "./Pagination";
+import ListGroup from "./ListGroup";
+import { Category, getCategories } from "../services/fakeCategoryService";
+
+const DEFAULT_CATEGORY: Category = { _id: "", name: "All Categories" };
 
 function Foods() {
   const [foods, setFoods] = useState(getFoods());
+  const [selectedPage, setSelectedPage] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState(DEFAULT_CATEGORY);
 
   function handleDelete(id: string) {
     const newFoods = foods.filter((food) => food._id !== id);
@@ -23,44 +30,59 @@ function Foods() {
   if (foods.length === 0) return <p>There are no foods in the database</p>;
 
   return (
-    <div className="container">
-      <p>Showing {foods.length} foods in the database</p>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Category</th>
-            <th scope="col">Price</th>
-            <th scope="col">Stock</th>
-            <th />
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {foods.map((food) => (
-            <tr key={food._id}>
-              <td>{food.name}</td>
-              <td>{food.category.name}</td>
-              <td>{food.price}</td>
-              <td>{food.numberInStock}</td>
-              <td>
-                <Favorite
-                  isFavored={Boolean(food.isFavored)}
-                  onFavor={() => handleFavor(food._id)}
-                />
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(food._id)}
-                >
-                  Delete
-                </button>
-              </td>
+    <div className="row container">
+      <div className="col-3">
+        <ListGroup
+          items={[DEFAULT_CATEGORY, ...getCategories()]}
+          selectedItem={selectedCategory}
+          onItemSelect={setSelectedCategory}
+        />
+      </div>
+      <div className="col">
+        <p>Showing {foods.length} foods in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Category</th>
+              <th scope="col">Price</th>
+              <th scope="col">Stock</th>
+              <th />
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {foods.map((food) => (
+              <tr key={food._id}>
+                <td>{food.name}</td>
+                <td>{food.category.name}</td>
+                <td>{food.price}</td>
+                <td>{food.numberInStock}</td>
+                <td>
+                  <Favorite
+                    isFavored={Boolean(food.isFavored)}
+                    onFavor={() => handleFavor(food._id)}
+                  />
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(food._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          totalCount={foods.length}
+          pageSize={4}
+          selectedPage={selectedPage}
+          onPageSelect={setSelectedPage}
+        />
+      </div>
     </div>
   );
 }
