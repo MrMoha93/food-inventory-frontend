5988e9436357dea8124434eb4ca715539258b30e
MohaK <mohakhalili@outlook.com>
2024-12-24 21:11:49 +0100

    added table body component

--- src/components/Foodstable.tsx
+++ src/components/Foodstable.tsx
@@ -1,5 +1,5 @@
 import { Food } from "../services/fakeFoodService";
-import Favorite from "./Favorite";
+import TableBody from "./TableBody";
 import TableHeader, { Column } from "./TableHeader";
 
 export interface SortColumn {
@@ -28,30 +28,7 @@ function FoodsTable({ foods, sortColumn, onSort, onDelete, onFavor }: Props) {
   return (
     <table className="table">
       <TableHeader onSort={onSort} sortColumn={sortColumn} columns={columns} />
-      <tbody>
-        {foods.map((food) => (
-          <tr key={food._id}>
-            <td>{food.name}</td>
-            <td>{food.category.name}</td>
-            <td>{food.price}</td>
-            <td>{food.numberInStock}</td>
-            <td>
-              <Favorite
-                isFavored={Boolean(food.isFavored)}
-                onFavor={() => onFavor(food._id)}
-              />
-            </td>
-            <td>
-              <button
-                className="btn btn-danger"
-                onClick={() => onDelete(food._id)}
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
+      <TableBody foods={foods} onDelete={onDelete} onFavor={onFavor} />
     </table>
   );
 }
