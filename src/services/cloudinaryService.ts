6e96c3598a88ba8d90e19210ee16d2492a6e416c
MohaK <mohakhalili@outlook.com>
2025-03-05 19:49:25 +0100

    uploaded all product images to cloudinary

--- src/services/cloudinaryService.ts
+++ src/services/cloudinaryService.ts
@@ -5,6 +5,7 @@ const cloudinaryURL = "https://api.cloudinary.com/v1_1/dawvv4e2m/image/upload";
 async function saveImage(file: File) {
   const formData = new FormData();
   formData.append("file", file);
+  formData.append("folder", "foods");
   formData.append("upload_preset", "Khalilis_Foods");
 
   const response = await axios.post(cloudinaryURL, formData, {
