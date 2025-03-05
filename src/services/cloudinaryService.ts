c459d7c1102bb172b2e89750f1bc3d70a0015008
MohaK <mohakhalili@outlook.com>
2025-03-05 19:16:41 +0100

    added cloudinary service

---
+++ src/services/cloudinaryService.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const cloudinaryURL = "https://api.cloudinary.com/v1_1/dawvv4e2m/image/upload";
+
+async function saveImage(file: File) {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "Khalilis_Foods");
+
+  const response = await axios.post(cloudinaryURL, formData, {
+    headers: { "x-auth-token": undefined },
+  });
+
+  return response.data.secure_url;
+}
+
+export default { saveImage };
